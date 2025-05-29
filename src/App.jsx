8280f39db3e80ecfe06ff539ce06d3b1586a6eaa
creation <creation2224@gmail.com>
2025-05-29 20:06:54 +0530

    made the structure

--- src/App.jsx
+++ src/App.jsx
@@ -4,30 +4,28 @@ import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [timer ,settimer ] = useState(0) ; 
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+     <div className='flex justify-center flex-col items-center h-screen w-screen '>
+      <div className='text-5xl font-bold p-8 '>
+      Walkifyy
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+      <div className='flex flex-col justify itme-center font-semibold text-2xl'>
+        <div className='flex  '>
+
+        <div>Set timer-</div>
+        <div>
+
+        <input type="number"  placeholder='Enter your time '/>
+        </div>
+        </div>
+         <div className='bg-red-600 text-center rounded cursor-pointer select-text px-4 py-2 '>Start Remainder</div>
+         <div className='text-center' mt-10px p-5 >Go For A walk </div>
+         
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+     </div>
     </>
   )
 }
